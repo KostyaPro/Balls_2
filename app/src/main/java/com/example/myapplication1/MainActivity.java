a9f3c2807594ec4bb6598e0ea584bcbd63328b82
KostyaPro <overite@mail.ru>
2022-04-22 15:33:03 +0500

    MyGraph

--- app/src/main/java/com/example/myapplication1/MainActivity.java
+++ app/src/main/java/com/example/myapplication1/MainActivity.java
@@ -10,6 +10,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         //setContentView(R.layout.activity_main);
-        setContentView(new MyGraph(this));
+        setContentView(new MyDraw(this));
     }
 }
